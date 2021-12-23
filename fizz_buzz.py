3bfbcd3ec03c10139ae7deaedbdcceb35ebbaad6
freshyolk <maichelle0@gmail.com>
2021-12-23 15:21:38 -0500

    changed from args to funcs

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,14 +1,14 @@
-import sys
 
-arguments = sys.argv[1:]
-n = arguments[0]
-print(n)
-n=int(n)
-type(int(n))
-if n % 3 == 0 and  n % 5 == 0:
-        print ("fizz buzz!")
-elif n % 5 == 0:
-        print ("buzz!")
-elif n % 3 == 0: 
-	print("fizz!")
+def fizz_buzz(n):
+	print(n)
+	n=int(n)
+	type(int(n))
+	if n % 3 == 0 and  n % 5 == 0:
+        	return "fizz buzz!"
+	elif n % 5 == 0:
+        	return "buzz!"
+	elif n % 3 == 0: 
+		return "fizz!"
 
+result = fizz_buzz(25)
+print(result)
