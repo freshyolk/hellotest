ca3d847c5d7e794a079cf3a44c7d6714ecd59074
freshyolk <maichelle0@gmail.com>
2021-12-23 15:31:22 -0500

    clean up & added test cases

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,8 +1,5 @@
 
 def fizz_buzz(n):
-	print(n)
-	n=int(n)
-	type(int(n))
 	if n % 3 == 0 and  n % 5 == 0:
         	return "fizz buzz!"
 	elif n % 5 == 0:
@@ -10,5 +7,10 @@ def fizz_buzz(n):
 	elif n % 3 == 0: 
 		return "fizz!"
 
-result = fizz_buzz(25)
-print(result)
+n_1 = 25
+n_2 = 6
+n_3 = 15
+print("called with {}, result is  {}".format( n_1, fizz_buzz(n_1) ))
+print("called with {}, result is  {}".format( n_2, fizz_buzz(n_2) ))
+print("called with {}, result is  {}".format( n_3, fizz_buzz(n_3) ))
+
