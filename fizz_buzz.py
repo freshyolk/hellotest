559c03d5c12446a3eadacb068afcc39d65553a1c
freshyolk <maichelle0@gmail.com>
2021-12-23 16:20:38 -0500

    added comments within python code

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,4 @@
+# fizz buzz function 
 
 def fizz_buzz(n):
 	if n % 3 == 0 and  n % 5 == 0:
@@ -9,6 +10,8 @@ def fizz_buzz(n):
 
 	return ""
 
+# testing the function
+ 
 n_1 = 25
 n_2 = 6
 n_3 = 15
@@ -16,11 +19,17 @@ print("called with {}, result is  {}".format( n_1, fizz_buzz(n_1) ))
 print("called with {}, result is  {}".format( n_2, fizz_buzz(n_2) ))
 print("called with {}, result is  {}".format( n_3, fizz_buzz(n_3) ))
 
+# for loop listing (vertical list)
+
 for n in range(11)[1:]: 
 	result = fizz_buzz(n) 
 	print (result)
- 
+
+# list comprehension with bracket list
+
 result = [fizz_buzz(n) for n in range(11)[1:]]
 print(result)
+
+# list comprehension with vertical list
 
 print("\n".join(result))
